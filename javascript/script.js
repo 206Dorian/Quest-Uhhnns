b451899e8c3bfe9d8b55da4384dea1116efe5e31
Dorian Birch <206dorian@gmail.com>
2023-01-13 15:04:30 -0600

    debuggingg questions

--- javascript/script.js
+++ javascript/script.js
@@ -2,13 +2,20 @@ var startButton = document.querySelector("#startbutton");
 
 var answerButtons = document.querySelector(".choices")
 
-var choiceOne =document.querySelector("#answer1")
-var choiceTwo =document.querySelector("#answer2")
-var choiceThree =document.querySelector("#answer3")
-var choiceFour =document.querySelector("#answer4")
-var choiceFive =document.querySelector("#answer5")
+var choiceOne = document.querySelector("#answer1")
+var choiceTwo = document.querySelector("#answer2")
+var choiceThree = document.querySelector("#answer3")
+var choiceFour = document.querySelector("#answer4")
+var choiceFive = document.querySelector("#answer5")
+
+var quizEl = document.querySelector("#question-container")
+
+var timerEl = document.querySelector(".timer");
+var mainEl = document.querySelector("main");
 
 
+var winCounter = 0;
+var loseCounter = 0;
 
 // var question = document.getElementById("questions");
 
@@ -19,9 +26,9 @@ var currentQuestions = {
 };
 var answer = true
 
-var timeLeft = 30;
+var timeLeft = 60;
 
-var points = 0;
+var points = 20;
 
 var questionCount = 0;
 
@@ -82,48 +89,57 @@ startGame = () => {
 
 getNewQuestion = () => {
   console.log(questionCount)
- var currentQuestion = questions[questionCount]
+  var currentQuestion = questions[questionCount]
   console.log(currentQuestion)
+  
   document.getElementById("question").textContent = currentQuestion.question;
 
+
+  quizEl.textContent=""
   for (i = 0; i < currentQuestion.choices.length; i++) {
-    choicesEl[i].textContent = currentQuestion.choices[i]
-    const optionButton = document.querySelector("answer"+i)
-    choicesEl[i].append(optionButton)
+    var choiceContainer = document.createElement("div")
+    var choicesEl = document.createElement("p")
+    choiceContainer.setAttribute("class", "choice-container")
+    choicesEl.setAttribute("class", "choices")
+    choicesEl.setAttribute("data-value", currentQuestion.choices[i])
+    choicesEl.textContent = currentQuestion.choices[i]
+    choicesEl.addEventListener("click", checkAnswer)
+
+    quizEl.appendChild(choiceContainer)
+    choiceContainer.appendChild(choicesEl)
+
+
+    // choicesEl[i].append(optionButton)
   }
   // questionCount++;
   // console.log(questionCount)
 }
 
-function checkAnswer (event){
-  let clickAnswer = event.target.textContent
+function checkAnswer() {
+  console.log(this.dataset.value)
   let correctAnswer = questions[questionCount].answer
 
-  if (clickAnswer===correctAnswer){
-    points+=5
-    
+  if (this.dataset.value === correctAnswer) {
+    points += 5
+
   }
-  else{
-    timeLeft-5
-points-=5
+  else {
+    timeLeft = timeLeft- 10
+    timerEl.textContent= timeLeft 
   }
-questionCount++
-if (questions.length>questionCount){
-  getNewQuestion()
-  
-} else {
-  console.log("quiz done")
-  //quizdone(cleartimer) 
+  questionCount++
+  if (questions.length > questionCount) {
+    getNewQuestion()
+
+  } else {
+    console.log("quiz done")
+    quizdone(cleartimer)
+  }
+
 }
-}
-// choices.forEach(choice =.> {
-//   var number = choice.dataset["number"];
-//   choice.innerText = currentQuestions["choice + number"];
-// })
 
 
-var timerEl = document.querySelector(".timer");
-var mainEl = document.querySelector("main");
+
 
 
 function startTimer() {
@@ -141,7 +157,7 @@ function startTimer() {
 
 // function startGame() {
 //   startTimer()
-  //in startGame remove class hide from question/answer container, needs an id in div, then hide start 
+//in startGame remove class hide from question/answer container, needs an id in div, then hide start 
 // };
 console.log(startGame)
 
